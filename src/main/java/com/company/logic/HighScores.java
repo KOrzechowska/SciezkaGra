3769a303634d6ecf8284a856b0c7e40d643b47c0
master <sam2441@wp.pl>
2017-01-22 11:41:19 +0100

    dodano nowe pliki

--- src/main/java/com/company/logic/HighScores.java
+++ src/main/java/com/company/logic/HighScores.java
@@ -2,23 +2,34 @@ package com.company.logic;
 
 import com.company.Game;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Set;
 
 public class HighScores {
-    private ArrayList<HighScore> highScoreList;
+    private ArrayList<HighScore> highScoreList = new ArrayList<>();
 
-    void loadHighScores() {
+    void saveHighScores() throws IOException {
+        FileOutputStream fos = new FileOutputStream("highscores");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(highScoreList);
+        oos.close();
+    }
 
+    void loadHighScores() throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream("highscores");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        highScoreList = (ArrayList<HighScore>) ois.readObject();
+        ois.close();
     }
 
     void addToHighScoreList() {
         Player player = Game.getGame().getPlayer();
         HighScore highScore = new HighScore(player.getScore(), player.getNick());
         highScoreList.add(highScore);
-        Collections.sort(highScoreList,new comparatorHighScore());
+        Collections.sort(highScoreList, new comparatorHighScore());
     }
 
     HighScore getHighScore(int i) {
